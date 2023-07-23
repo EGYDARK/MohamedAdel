647fcbc1c5cff0113fc844f7ab83f3d4eb92409b
Hady <137319838+EGYDARK@users.noreply.github.com>
2023-07-23 05:16:19 -0700

    Add files via upload

---
+++ js/Email.js
@@ -0,0 +1,12 @@
+/*
+Email.send({
+    Host : "smtp.youssefadel9888@gmail.com",
+    Username : "youssefadel9888@mgail.com",
+    Password : "gmisnoop123456789a",
+    To : 'yousefadel66622@gmail.com',
+    From : document.getElementById('email').value,
+    Subject : "New Contact Form",
+    Body : "And this is the body"
+}).then(
+message => alert(message)
+);*/
---
+++ js/typing.js
@@ -0,0 +1,7 @@
+/* =============================== typing Animation =================================== */
+var typed = new Typed(".typing", {
+    strings:["Media Buyer", "Digital Marketing Specialist","Media Buyer", "Youtube Manager" ],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
